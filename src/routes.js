4e7780533d9bc5ea33506233cea30f15acb9b06b
Bruno Parreira <brunoparreira77@gmail.com>
2023-10-15 16:01:22 -0300

    Delete 404 and 500

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,7 @@
 import React from 'react'
 
 const Home = React.lazy(() => import('./views/home/Home'))
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
-const routes = [
-  { path: '/', exact: true, name: 'Home', element: Home },
-  { path: '/404', name: 'Page 404', element: Page404 },
-  { path: '/500', name: 'Page 500', element: Page500 },
-]
+const routes = [{ path: '/', exact: true, name: 'Home', element: Home }]
 
 export default routes
